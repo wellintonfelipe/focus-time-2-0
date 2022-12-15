9d0128979667a94fc2a774d1d21b76e8b2742062
Wellinton Felipe <wellintonfelipe@hotmail.com.br>
2022-12-15 19:16:30 -0300

    Add class dark style.css

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,17 @@
+const html = document.querySelector("html");
+
 const btnThemeDark = document.querySelector(".dark-mode");
 const btnThemeLight = document.querySelector(".light-mode");
 
 btnThemeLight.addEventListener("click", () => {
+  html.classList.add("dark");
   btnThemeDark.classList.remove("hidden");
   btnThemeLight.classList.add("hidden");
 });
 
 btnThemeDark.addEventListener("click", () => {
+  html.classList.remove("dark");
+
   btnThemeDark.classList.add("hidden");
   btnThemeLight.classList.remove("hidden");
 });
